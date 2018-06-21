c954f31f1c289e17d8caef680ca2d11c592b5542
Rishabh Chandra <rchandra@Rishabhs-MacBook-Pro.local>
2018-06-21 11:38:19 -0700

    Week 2: passed newline test cases

--- str_adder.py
+++ str_adder.py
@@ -2,8 +2,11 @@ def add(str):
     if str == "":
         return 0
     s = str.split(",")
+    sf = []
+    for string in s:
+        sf.extend(string.split("\n"))
     sum = 0
-    for val in s:
+    for val in sf:
         sum += int(val)
 
     return sum
