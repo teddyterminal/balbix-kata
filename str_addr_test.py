44f0bae2deb2370a69f625c0663bed2701a62fe0
Rishabh Chandra <rchandra@Rishabhs-MacBook-Pro.local>
2018-06-28 11:45:58 -0700

    removed negative tests & added semicolon delimiter

--- str_addr_test.py
+++ str_addr_test.py
@@ -3,35 +3,30 @@ from str_adder import add
 
 class TestStringAdder(unittest.TestCase):
 
-    def test_empty(self):
+    def test_empty_equals_zero(self):
         self.assertEqual(add(""), 0)
 
     def test_one_number(self):
         self.assertEqual(add("2"), 2)
 
-    def test_one_negative_number(self):
-        self.assertEqual(add("-1"), -1)
-
     def test_two_numbers(self):
         self.assertEqual(add("5,7"), 12)
 
-    def test_two_numbers_onepos_oneneg(self):
-        self.assertEqual(add("-3, 5"), 2)
-
-    def test_two_numbers_bothneg(self):
-        self.assertEqual(add("-3,-3"), -6)
-
     def test_many_numbers(self):
-        self.assertEqual(add("4,5,6,-4"), 11)
+        self.assertEqual(add("4,5,6"), 15)
 
     def test_many_numbers_2(self):
         self.assertEqual(add("0,1,2,6"), 9)
 
-    def test_newline(self):
+    def test_newline_separator_sum(self):
         self.assertEqual(add("0\n1\n2"), 3)
 
-    def test_newline_commas(self):
-        self.assertEqual(add("0,1\n6\n-4,-5"), -2)
+    def test_newline_and_commas_separator(self):
+        self.assertEqual(add("0,1\n6\n4,5"), 16)
+
+    def test_new_delimiter(self):
+        self.assertEqual(add("//;\n0;1;4"), 5)
+
 
 if __name__ == '__main__':
     unittest.main()
