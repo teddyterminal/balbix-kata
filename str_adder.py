67d3992f9629199cf13de61c66f3e75b0154017d
Rishabh Chandra <rchandra@rishabhs-mbp.attlocal.net>
2018-06-28 20:12:52 -0700

    added backslashes to tests and passed testcases for long delimiters

--- str_adder.py
+++ str_adder.py
@@ -3,13 +3,17 @@ import re
 def add(s):
     delim = [",","\n"]
     if s[0:2] == "//":
-        delim.append(s[2])
-        s = s[4:]
+        if s[2] != "[":
+            delim.append(s[2])
+            s = s[4:]
+        else:
+            delim.append(s[3:s.index("]")])
+            s = s[s.index("]")+1:]
 
     if s == "":
         return 0
     delim = "|".join(delim)
-    s = re.split(delim, s)
+    s = list(filter(None, re.split(delim, s)))
 
     sum = 0
     negatives = []
